7b07b43dd5892e145a7d9f656a40cd5ab41fcd1f
Min Chul Kim <minchulkim87@gmail.com>
2020-05-15 17:36:05 +1000

    Some tidying up of code and setup CA module

---
+++ markstattools/ca.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import shutil
+import glob
+from typing import Callable, Iterator
+import json
+import pyarrow
+from datetime import timedelta, date
+import numpy as np
+import pandas as pd
+import dask
+import dask.dataframe as dd
+import pandas_read_xml as pdx
+from pandas_read_xml import auto_separate_tables
+
+
+save_path = './downloads/ca'
+backup_path = './backup/ca'
+temp_path = './temp/ca'
+data_path = './data/ca'
+upload_folder_path = './upload/ca'
+
+
+# -------------------------------------------------------------------------------------
+# These functions will help download all the files and save them locally.
+# -------------------------------------------------------------------------------------
+
+# TODO
+
+# -------------------------------------------------------------------------------------
+# These functions will help read the downloaded files and combine them.
+# -------------------------------------------------------------------------------------
+
+# TODO
+
+# These functions make the individual updates happen in a "safer" way by saving to temp folder and replacing the old data only after success.
+
+def backup() -> None:
+    shutil.copytree(data_path, backup_path)
+
+
+def commit() -> None:
+    shutil.rmtree(data_path)
+    shutil.copytree(temp_path, data_path)
+    # TODO: updating currency of data information
+    shutil.rmtree(temp_path)
+
+
+def rollback() -> None:
+    shutil.rmtree(data_path)
+    shutil.copytree(backup_path, data_path)
+    shutil.rmtree(backup_path)
+
+
+
+# -------------------------------------------------------------------------------------
+# This function will automate everything.
+# -------------------------------------------------------------------------------------
+
+
+def update_all() -> None:
+    # TODO
+    print('Not implemented yet.')
+
+
+def initialise():
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    if not os.path.exists(backup_path):
+        os.makedirs(backup_path)
+    if not os.path.exists(temp_path):
+        os.makedirs(temp_path)
+    if not os.path.exists(upload_folder_path):
+        os.makedirs(upload_folder_path)
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+
+if __name__ == '__main__':
+    initialise()
+    update_all()
